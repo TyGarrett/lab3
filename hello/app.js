50bae75e61c8f3d57b7a38ff798767819c60c52c
Patrick Garrett <pgarrett@ucsd.edu>
2019-01-24 11:47:46 -0800

    h

--- hello/app.js
+++ hello/app.js
@@ -11,6 +11,8 @@ var handlebars = require('express3-handlebars')
 var hello = require('./routes/hello');
 
 var app = express();
+var hello = require('./routes/hello');
+app.get('hello/:username', hello.view);
 
 // all environments
 app.set('port', process.env.PORT || 3000);
